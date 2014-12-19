9ddd49df32a0140d2a4785f13f2b016ce7aa682a
堀江 麗 <bellerei@horie-no-MacBook-Air.local>
2014-12-19 18:01:06 +0900

    editing journals

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def post_image_for(post, size = '208')
+    image_tag "/docs/#{post.image}", size: size, class: :gravatar
+  end
 end
---
+++ db/migrate/20141219073459_add_column_to_posts.rb
@@ -0,0 +1,5 @@
+class AddColumnToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :image, :string
+  end
+end
